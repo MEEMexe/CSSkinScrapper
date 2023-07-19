a8a85ebc4edabf148581041cce069b02a8b55801
Niklas Arndt <niklasarndt65@gmail.com>
2023-07-19 09:57:41 +0200

    Projektdateien hinzufügen.

---
+++ ExcelInterop.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace CSSkinScrapper
+{
+    internal class ExcelInterop
+    {
+        private static string path = "C:\\Users\\nikla\\Desktop\\CS-Skins\\CSGO-Skins.xlsx";
+
+        public static void WriteExcel(string[,] skinarray)
+        {
+            int skinCount = skinarray.GetLength(0) + 2;
+
+            Application excelApp = new Application();
+            Workbook wb = excelApp.Workbooks.Open(path);
+            Worksheet ws = wb.Worksheets[1];
+
+            Range test = ws.Range["I3:I" + skinCount];
+            test.set_Value(XlRangeValueDataType.xlRangeValueDefault, skinarray);
+
+            wb.Save();
+            wb.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSSkinScrapper
+{
+    internal class Program
+    {
+        static string baseUrl =
+            "http://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name=";
+
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            int skinCount = Skins.skinCount;
+            string[,] priceArray = new string[skinCount, 1];
+
+            for (int i = 0; i < skinCount; i++)
+            {
+                string form = " price:\t";
+                string skinname = Skins.skins[i, 0];
+                string price = GetPrice(Skins.skins[i, 1]).GetAwaiter().GetResult();
+
+                if (skinname.Length < 9)
+                    form += "\t";
+
+                priceArray[i, 0] = price;
+
+                Console.WriteLine(skinname + form + price);
+            }
+
+            ExcelInterop.WriteExcel(priceArray);
+        }
+
+        static async Task<string> GetPrice(string skinpath)
+        {
+            string requestUrl = baseUrl + skinpath;
+
+            HttpResponseMessage response = await client.GetAsync(requestUrl);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception();
+            }
+
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            int i = responseString.IndexOf("lowest_price");
+            string price = responseString.Substring(i + 15, 4);
+
+            return price;
+        }
+    }
+}
