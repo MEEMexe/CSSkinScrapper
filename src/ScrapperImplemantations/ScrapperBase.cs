a40eaa2d887942c38006c495085c084cb57602a8
Niklas Arndt <niklasarndt65@gmail.com>
2023-10-13 00:22:49 +0200

    Added ScrapperBase

---
+++ src/ScrapperImplemantations/ScrapperBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System;
+using System.Collections.Generic;
+
+namespace CSSkinScrapper.ScrapperImplemantations
+{
+    internal abstract class ScrapperBase
+    {
+        private HttpClient client;
+
+        protected ScrapperBase(string baseUrl)
+        {
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+        }
+
+        public abstract string GetUrl(Skin skin);
+        public abstract double GetPrice(Skin skin);
+        public abstract double[] GetPriceArray(List<Skin> skinList);
+
+        protected HttpResponseMessage GetResponse(string skinpath)
+        {
+            return client.GetAsync(client.BaseAddress + skinpath).GetAwaiter().GetResult();
+        }
+    }
+
+    public class BadRequestException : Exception { }
+    public class SkinNotFoundException : Exception
+    {
+        public SkinNotFoundException(string message) : base(message) { }
+    }
+}
