6cdc63e91a6fcfb9393006d0a7906bcfe403f2ca
Niklas Arndt <niklasarndt65@gmail.com>
2024-04-10 00:31:13 +0200

    Created new sln

--- src/Program.cs
+++ src/Program.cs
@@ -1,40 +1,11 @@
-﻿using CSSkinScrapper.ScrapperImplemantations;
-using System;
-
-namespace CSSkinScrapper
+﻿namespace CSSkinScrapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            ScopeToTriggerGC();
-
-            //Wait for Excel Process to finalize
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-        }
-
-        static void ScopeToTriggerGC()
-        {
-            //Load user settings
-            var jsonInterop = new JSONInterop();
-            var saveFile = jsonInterop.Load();
-
-            //new skins?
-            bool newSkin = UserInterface.AskNewSkin(saveFile);
-
-            //save savefile if new skins got added
-            if (newSkin)
-                jsonInterop.Save(saveFile);
-
-            //get newest prices
-            Console.WriteLine("Getting Prices:\n");
-            double[] prices = SkinScrapper.GetPriceArray(saveFile.skinList);
-
-            //Write to excel
-            Console.WriteLine("\nWriting to Excelsheet...");
-            var exelInterop = new ExcelInterop(saveFile);
-            exelInterop.WritePrices(prices, newSkin);
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
         }
     }
 }
